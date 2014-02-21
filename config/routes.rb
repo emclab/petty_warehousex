eca4d2edb71a41259ec5c57bfbda8388ec305214
jc <emclab2011@gmail.com>
2014-02-20 19:05:49 -0600

    JC: added @view_in_config and @max_pagination to application controller. spec passes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 PettyWarehousex::Engine.routes.draw do
 
-  resources :items
+  resources :items do
+    collection do
+      get :search
+      put :search_results
+      #get :stats
+      #put :stats_results 
+    end
+  end
   
   root :to => 'items#index'
 end
