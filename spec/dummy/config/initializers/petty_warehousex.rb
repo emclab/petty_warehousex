f6960bf29721b5029596bcaf511f894296f0d685
JC <jc@email.com>
2016-05-20 11:27:49 -0500

    JC: removed an extra mattr from init file. spec passes.

--- spec/dummy/config/initializers/petty_warehousex.rb
+++ spec/dummy/config/initializers/petty_warehousex.rb
@@ -1,5 +1,4 @@
 PettyWarehousex.supplier_class = 'Supplierx::Supplier'
-PettyWarehousex.index_checkout_path = 'item_checkoutx.checkouts_path(:item_id => r.id)'
 PettyWarehousex.project_class = 'ExtConstructionProjectx::Project'
 PettyWarehousex.purchase_order_class = 'Commonx::MiscDefinition'  #need to change if there is purchase order class.
 PettyWarehousex.material_item_class = 'BaseMaterialx::Part'
